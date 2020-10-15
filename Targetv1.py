dad21e6f3a6807d9f07a7051fdd977865f28c8f4
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-15 17:10:22 +0800

    Update Targetv1.py

--- Targetv1.py
+++ Targetv1.py
@@ -1,12 +1,3 @@
-bi='\033[34;1m7' #biru
-ij='\033[32;1m' #ijo
-pr='\033[35;1m' #purple
-cy='\033[36;1m' #cyan
-me='\033[31;1m' #merah
-pu='\033[37;1m' #putih
-ku='\033[33;1m' #kuning
-or='\033[1;38;5;208m' #Orang
-clear
 import requests
 import threading
 # import urllib.request
