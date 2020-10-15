280ce4e1f4d5d416c94305141866391a36bf66b1
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-15 17:06:30 +0800

    Update and rename fb.py to Targetv1.py

--- Targetv1.py
+++ Targetv1.py
@@ -1,3 +1,12 @@
+bi='\033[34;1m7' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orang
+clear
 import requests
 import threading
 # import urllib.request
@@ -45,13 +54,13 @@ def function(email,passw,i):
 		return True
 	return False
 
-print('\n---------- Welcome To Facebook BruteForce ----------\n')
+print('\033[32;1m<<•  @ROOT  •>>>')
 file=open('passwords.txt','r')
 
 email=input('Enter Email/Username : ')
 
-print("\nTarget Email ID : ",email)
-print("\nTrying Passwords from list ...")
+print("\nTarget Email/Id : ",email)
+print("\nMENCOBA KE AMAN AKUN/ATTACK LIST ...")
 
 i=0
 while file:
